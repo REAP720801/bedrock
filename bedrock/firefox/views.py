4278a308e5027c2ec9e7d52f9f33af3f8021b790
Stephanie Hobson <sjhobson@gmail.com>
2018-07-17 16:15:36 -0700

    First pass
    - image assets
    - split files to work from central base

--- bedrock/firefox/views.py
+++ bedrock/firefox/views.py
@@ -547,13 +547,20 @@ def download_thanks(request):
         template = 'firefox/new/wait-face/scene2.html'
     elif lang_file_is_active('firefox/new/reggiewatts', locale) and experience == 'reggiewatts':
         template = 'firefox/new/reggie-watts/scene2.html'
-    elif locale == 'de' and experience == 'berlin':
-        if variant == 'b':
-            template = 'firefox/new/berlin/scene2-b.html'
+    elif locale == 'de':
+        if experience == 'berlin':
+            if variant == 'b':
+                template = 'firefox/new/berlin/scene2-b.html'
+        elif experience == 'herz':
+            template = 'firefox/new/berlin/scene2-herz.html'
+        elif experience == 'speed':
+            template = 'firefox/new/berlin/scene2-speed.html'
+        elif experience == 'privacy':
+            template = 'firefox/new/berlin/scene2-privacy.html'
+        elif experience == 'oys':
+            template = 'firefox/new/berlin/scene2-oys.html'
         else:
             template = 'firefox/new/berlin/scene2.html'
-    elif locale == 'de' and experience == 'herz':
-        template = 'firefox/new/berlin/scene2-herz.html'
     elif locale == 'en-US':
         if experience in ['portland', 'forgood']:
             template = 'firefox/new/portland/scene2.html'
@@ -598,13 +605,20 @@ def new(request):
             template = 'firefox/new/wait-face/scene1.html'
         elif lang_file_is_active('firefox/new/reggiewatts', locale) and experience == 'reggiewatts':
             template = 'firefox/new/reggie-watts/scene1.html'
-        elif locale == 'de' and experience == 'berlin':
-            if variant == 'b':
-                template = 'firefox/new/berlin/scene1-b.html'
+        elif locale == 'de':
+            if experience == 'berlin':
+                if variant == 'b':
+                    template = 'firefox/new/berlin/scene1-b.html'
+            elif experience == 'herz':
+                template = 'firefox/new/berlin/scene1-herz.html'
+            elif experience == 'speed':
+                template = 'firefox/new/berlin/scene1-speed.html'
+            elif experience == 'privacy':
+                template = 'firefox/new/berlin/scene1-privacy.html'
+            elif experience == 'oys':
+                template = 'firefox/new/berlin/scene1-oys.html'
             else:
                 template = 'firefox/new/berlin/scene1.html'
-        elif locale == 'de' and experience == 'herz':
-            template = 'firefox/new/berlin/scene1-herz.html'
         elif locale == 'en-US':
             if experience in ['portland', 'forgood']:
                 template = 'firefox/new/portland/scene1.html'
